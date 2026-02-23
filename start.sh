2532fc9ef1c8634cfb566d8db09ed45e59ae7920
Alton <al648228665@gmail.com>
2026-02-22 19:16:19 -0800

    make it work

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ fi
 
 if [ -n "$RPC_SECRET" ]; then
     sed -i '/^rpc-secret=/d' $conf_path/aria2.conf
-    printf 'rpc-secret=%s\n' "${RPC_SECRET}" >>$conf_path/aria2.conf
+    printf '\nrpc-secret=%s\n' "${RPC_SECRET}" >>$conf_path/aria2.conf
 
     if [ -n "$EMBED_RPC_SECRET" ]; then
         echo "Embedding RPC secret into ariang Web UI"
@@ -32,7 +32,7 @@ fi
 
 touch $conf_path/aria2.session
 
-if [[ -n "$ARIA2RPCPORT" ]]; then
+if [ -n "$ARIA2RPCPORT" ]; then
     echo "Changing rpc request port to $ARIA2RPCPORT"
     sed -i "s/6800/${ARIA2RPCPORT}/g" $ariang_js_path
 fi
@@ -40,7 +40,7 @@ fi
 userid="$(id -u)" # 65534 - nobody, 0 - root
 groupid="$(id -g)"
 
-if [[ -n "$PUID" && -n "$PGID" ]]; then
+if [ -n "$PUID" ] && [ -n "$PGID" ]; then
     echo "Running as user $PUID:$PGID"
     userid=$PUID
     groupid=$PGID
@@ -49,5 +49,5 @@ fi
 chown -R "$userid":"$groupid" $conf_path
 chown -R "$userid":"$groupid" $data_path
 
-caddy start -config /usr/local/caddy/Caddyfile -adapter=caddyfile
+caddy start --config /usr/local/caddy/Caddyfile --adapter=caddyfile
 su-exec "$userid":"$groupid" aria2c "$@"
